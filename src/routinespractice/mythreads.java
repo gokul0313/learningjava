8a613a5f3e985d013827a1591f3706ed7ee362ba
gokul j <gokuljp101@gmail.com>
2026-08-12 10:25:46 +0530

    working code

--- src/routinespractice/mythreads.java
+++ src/routinespractice/mythreads.java
@@ -1,54 +1,61 @@
 package routinespractice;
-import java.lang.Thread;
+
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-public class mythreads  {
-    
 
-    public static void Printthread() throws Exception{
-        BlockingQueue<String> ch=new LinkedBlockingQueue<>();
+public class mythreads {
+
+    public static void Printthread() throws Exception {
+        BlockingQueue<String> ch = new LinkedBlockingQueue<>();
+
         for (int i = 0; i < 5; i++) {
-        
+            final int index = i; // must be effectively final to use in lambda
 
-            new Thread(()->{
-              ch.put(computesomething(i));
+            new Thread(() -> {
+                try {
+                    ch.put(computesomething(index));
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    System.err.println("Interrupted while putting: " + e);
+                }
             }).start();
-            
-           System.err.println("Resulr :",ch.get());  
 
+            String result = ch.take(); // blocks until an item is available
+            System.err.println("Result: " + result);
         }
-
     }
 
-    public static String computesomething(int pInput )  {
-long lDuration;
+    public static String computesomething(int pInput) {
+        long lDuration;
         switch (pInput) {
+            case 0:
+                lDuration = 1500;
+                break;
             case 1:
-                lDuration=2000;
+                lDuration = 2000;
                 break;
-             case 2:
-                lDuration=3000;
+            case 2:
+                lDuration = 3000;
                 break;
-             case 3:
-                lDuration=5000;
+            case 3:
+                lDuration = 5000;
                 break;
-                 case 4:
-                lDuration=1000;
-                break;
-                 case 5:
-                lDuration=6000;
+            case 4:
+                lDuration = 1000;
                 break;
             default:
                 throw new AssertionError();
         }
         try {
-            
             Thread.sleep(lDuration);
-        } catch (Exception e) {
-            System.err.println("Error in method: "+e);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Error in method: " + e);
         }
-        return "hi "+pInput;
+        return "hi " + pInput;
     }
 
-    
+    // public static void main(String[] args) throws Exception {
+    //     Printthread();
+    // }
 }
